ec141054977bfc0bacba5a7a3410f06e7c21a5b4
TheRealLPG <dam.cg30@gmail.com>
2025-04-28 14:42:11 +0200

    Cypress fix

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:8000/')
+    cy.visit('http://127.0.0.1:8080/')
   })
 })
 
-/*
-
 describe('Create and connect to an account', () => {
   it('Visits the Oc commerce site', () => {
-    cy.visit('http://127.0.0.1:8000/home')
+    cy.visit('http://127.0.0.1:8080/home')
 
     // User is able to create an account an to be redirect to login pages
 
@@ -33,14 +31,14 @@ describe('Create and connect to an account', () => {
   })
 })
 
-*/
-
-// Can fail, because of line 68
+// Can fail, because of last line
 describe('Put item in favourite', () => {
   it('Connect to OC commerce and put in favourite', () => {
 
+    /*
+    // To uncomment if the previous account is already created and launched in local
     // In this test you should load the home url and connect with the previous account
-    cy.visit('http://127.0.0.1:8000/home')
+    cy.visit('http://127.0.0.1:8080/home')
     cy.contains('LOGIN').click()
     cy.url().should('include', '/user/login')
     cy.get('[id^=your_name]').type('fakeuser')
@@ -48,6 +46,7 @@ describe('Put item in favourite', () => {
     cy.get('label[for="agree-term"]').click()
     cy.get('[id^=signin]').click()
     cy.url().should('include', '/home')
+    */
 
     // You will go to favourite pages to make sure there is no favourite
     cy.contains('FAVOURITE').click()
@@ -58,15 +57,37 @@ describe('Put item in favourite', () => {
     cy.url().should('include', '/home')
 
     // You will add an item to favourite
-    cy.get('[id^=favBtn]').click({multiple: true}) // test on all items - I don't know how to ensure that all are hearted or unhearted
+    cy.get('[id^=favBtn]').click({multiple: true});
 
     // You will go to favourite pages to confirm item is here
     cy.contains('FAVOURITE').click()
     cy.url().should('include', '/favourite')
 
     // You will then delete the item an check it has been successfully deleted
-    cy.get('a[onclick^=markFavourite]').click({multiple: true}) // test on all items - I can only remove 1 with this command
+    // .get('a[onclick^="markFavourite"]').click({multiple: true})
+    // cy.get('#favBtn').click()
 
+    // should work if there is at least 1 element in favourite
+    function clickAllFavourites() {
+      cy.get('a[onclick^="markFavourite"]').then(($elements) => {
+        if ($elements.length > 0) {
+          cy.wrap($elements[0])
+            .click()
+    
+          cy.wait(1000)
+          
+          // End condition, if there was exactly 1 element left in the favourite, we will not launch the function again
+          // because the list is, at this state, empty
+          if($elements.length > 1) {
+            clickAllFavourites();
+          }
+        }
+      });
+    }
+    
+    // Et on lance :
+    clickAllFavourites();
+    
 
   })
 })
